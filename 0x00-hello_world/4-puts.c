4e303e9c86a31964d7ed7bfa6d82715f326e4b0c
Yordi-Dereje <yordadereje@gmail.com>
2022-03-10 00:16:53 -0800

    fifth one I think

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,7 @@
-#!/bin/bash
-puts("Programming is like building a multilingual puzzle")
+#include<stdio.h>
+int main(){
+
+	char str[]="\"Programming is like building a multilingual puzzle";
+	puts(str);
+	return 0;
+}
