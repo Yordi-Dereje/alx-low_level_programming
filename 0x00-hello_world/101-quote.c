19f78638510aecc6a51e480ea7b0a71791858083
Yordi-Dereje <yordadereje@gmail.com>
2022-03-10 13:04:57 -0800

    uff

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 /*
- * main - Entry pont
+ * main - Entry point
  * Return: Always 0 (success)
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
