085b5db3e2ac195553050cb8f5758dd8bf7e93d2
Yordi-Dereje <yordadereje@gmail.com>
2022-03-10 07:06:28 -0800

    oremaniya

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
-int main(void) /*this is the main function*/
+/*This program is printing out a text using puts*/
+int main(void)
 {
 	char str[] = "\"Programming is like building a multilingual puzzle";
 	puts(str);
-	return 0;
+	return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
+/*This function is to print out a string with printf*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,");
-	return 0;
+	return (0);
 }
