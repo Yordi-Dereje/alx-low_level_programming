9de9b74c94974f3f3d38cb5a42cc9054744cd50a
Yordi-Dereje <yordadereje@gmail.com>
2022-03-10 08:57:20 -0800

    hi

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/*
+ * main - Entry pont
+ * Return: 1 (success)
+ */
+int main(void)
+{
+	fprint(stdout,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
