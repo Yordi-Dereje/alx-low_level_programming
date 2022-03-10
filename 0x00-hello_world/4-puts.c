21bc714ce25a39cd0d88e8e1d67670db118d0f92
Yordi-Dereje <yordadereje@gmail.com>
2022-03-10 00:30:43 -0800

    betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,7 @@
 #include<stdio.h>
 int main(void) /*this is the main function*/
 {
-
-	char str[]="\"Programming is like building a multilingual puzzle";
+	char str[] = "\"Programming is like building a multilingual puzzle";
 	puts(str);
 	return 0;
 }
