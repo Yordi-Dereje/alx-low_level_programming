cf51174c2d832c21f28b7147111fd33e2ae81d6e
Yordi-Dereje <yordadereje@gmail.com>
2022-03-11 12:13:16 -0800

    hi

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -15,13 +15,16 @@ int main(void)
 		{
 			for (o = 48; o <= 57; o++)
 			{
-				if (m < n && n < o)
+				if (m > n && n > o)
 				{
 					putchar(m);
 					putchar(n);
 					putchar(o);
+					if (m != 55)
+					{
 					putchar(',');
 					putchar(' ');
+					}
 				}
 			}
 		}
