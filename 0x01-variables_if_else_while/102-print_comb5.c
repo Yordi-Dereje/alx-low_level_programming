ea57642178fb19f99cbbf6998cac27d7e1b5118b
Yordi-Dereje <yordadereje@gmail.com>
2022-03-11 10:07:47 -0800

    one of these

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	int b;
+	int c = 0;
+	int d;
+
+	for (a = 0; a <= 99; a++)
+	{
+		for (b = 0; b <= 99; b++)
+		{
+			if (a != b && a < b)
+			{
+				if (a < 10)
+				{
+					putchar((c % 10) + '0');
+				}
+				putchar((a % 10) + '0');
+				putchar(' ');
+				if (b < 10)
+				{
+					putchar((c % 10) + '0');
+				}
+				putchar((b % 10) + '0');
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
+					
