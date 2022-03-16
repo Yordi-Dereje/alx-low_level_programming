7c8c1657805db97d58f7b34651c0beff39e96d90
Yordanos Dereje <yordadereje@gmail.com>
2022-03-16 16:33:14 +0300

    hi

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -5,11 +5,10 @@
  * Return: Always 0 (Success)
  */
 
-void positive_or_negative(int i)
+int main(void)
 {
-	if (i < 0)
-		printf("%d is negative", i);
-	else
-		printf("%d is positive", i);
-	printf("/n");
+
+	positive_or_negative(i);
+	return 
 }
+
