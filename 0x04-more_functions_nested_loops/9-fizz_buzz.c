2f78bf1cc8034416fe51ce55339bfec778022f47
Yordanos Dereje <yordadereje@gmail.com>
2022-03-18 00:41:01 +0300

    hi

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,15 +2,15 @@
 /**
  * main - Program starts here
  *
- * Return: Always 0
+ * Return: Always 0.
  */
 int main(void)
 {
 	int i;
-
+	
 	for (i = 1; i < 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
+		if (i % 15 == 0)
 			printf("FizzBuzz ");
 		else if (i % 3 == 0)
 			printf("Fizz ");
@@ -18,7 +18,6 @@ int main(void)
 			printf("Buzz ");
 		else
 			printf("%d ", i);
-	
 	}
 	printf("Buzz\n");
 	return (0);
