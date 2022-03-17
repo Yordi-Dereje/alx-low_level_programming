6b2a338849e5ddf64fa74c9e9ce24e6a79863b41
Yordanos Dereje <yordadereje@gmail.com>
2022-03-18 00:28:48 +0300

    hi

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,6 +1,9 @@
 #include "main.h"
 /**
  * print_square - print a square
+ *
+ * @size: variable for the dimensions
+ *
  * Return: nothing
  */
 void print_square(int size)
@@ -11,9 +14,9 @@ void print_square(int size)
 
 	if (size > 0)
 	{
-		for (a = 1; a <= size; a++)
+		for (a = 0; a < size; a++)
 		{
-			for (b = 1; b <= a; b++)
+			for (b = 0; b < size; b++)
 			{
 				_putchar('#');
 			}
