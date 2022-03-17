77ce3eda7ca69afac98dfaa2d5470997d28390de
Yordanos Dereje <yordadereje@gmail.com>
2022-03-18 00:12:57 +0300

    hi

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,8 +1,10 @@
 #include "main.h"
 /**
  * mul - multiply 2 integers
- * @a - variable 1
- * @b - variable 2
+ * 
+ * @a: variable 1
+ * @b: variable 2
+ * 
  * Return: the value
  */
 int mul(int a, int b)
