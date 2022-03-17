420183263845ae7bf24ef4e34a6cce95828bef9e
Yordanos Dereje <yordadereje@gmail.com>
2022-03-17 21:35:09 +0300

    hi

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - draw straight line
+ * Return: nothing
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (i > 0)
+	{
+		for (i = 1; i <= n; i++)
+	                _putchar('_');
+
+		_putchar('\n');
+	}
+	else
+		_putchar('\n');
+}
