8c84f1ae43c3c00e462d551f14c3acd54f046458
Yordanos Dereje <yordadereje@gmail.com>
2022-03-18 06:56:43 +0300

    hi

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,17 +7,16 @@
 void more_numbers(void)
 {
 	int i;
-
 	int x;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (x = 0; x <= 14; x++)
-		{	
-		if (x > 9)
-			_putchar((x / 10) + '0');
-		_putchar((x % 10) + '0');
+		{
+			if (x > 9)
+				_putchar((x / 10) + '0');
+			_putchar((x % 10) + '0');
 		}
 		_putchar('\n');
-	}		
+	}
 }
