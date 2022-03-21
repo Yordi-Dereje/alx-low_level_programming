03506c9dd0ef10516a8074f6a00d2bdb0ffea88b
Yordanos Dereje <yordadereje@gmail.com>
2022-03-21 17:47:01 +0300

    4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,14 +8,14 @@
  */
 void print_rev(char *s)
 {
-	int a = 0;
-	int b = a;
+	int a;
+	int b;
 
-	for(a = 0; *s != '\0'; a++)
+	for (a = 0; *s != '\0'; a++)
 	{
 		s++;
 	}
-	
+
 	for (b = a; s[b] >= 0; b--)
 	{
 		_putchar(s[b]);
