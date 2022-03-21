e28e66107c7a798877866a537332880a55e6955b
Yordanos Dereje <yordadereje@gmail.com>
2022-03-21 14:01:12 +0300

    third task

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include<main.h>
+/**
+ * _strlen(char *s) - a function that returns the length of a string
+ *
+ * @s: the passed string
+ *
+ * return: the value
+ */
+int _strlen(char *s)
+{
+	int a = 0;
+
+	while(*s != '\0')
+	{
+		*s++;
+		a++;
+	}
+	return (a);
+}
