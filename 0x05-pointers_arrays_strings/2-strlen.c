907fe9bd5fc2b4ead3173e0ae473685dac7dc87e
Yordanos Dereje <yordadereje@gmail.com>
2022-03-21 19:23:37 +0300

    2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _strlen(char *s) - a function that returns the length of a string
+ * _strlen - a function that returns the length of a string
  *
  * @s: the passed string
  *
