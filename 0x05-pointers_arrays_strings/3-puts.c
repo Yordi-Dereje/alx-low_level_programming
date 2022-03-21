76b19aa3b4b7c9eee6539f16444a1020bc2f6826
Yordanos Dereje <yordadereje@gmail.com>
2022-03-21 14:11:51 +0300

    fourth

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include<main.h>
+/**
+ * _puts - prints a string
+ *
+ * @str: the passed string
+ *
+ * Return: nothing
+ */
+void _puts(char *str)
+{
+	for( int a = 0; str[a] != 0; a++)
+		_putchar(str[a]);
+	_putchar('\n');
+}
