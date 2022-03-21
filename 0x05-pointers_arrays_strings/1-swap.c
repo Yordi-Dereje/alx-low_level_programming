de028d1f7447e7bb94da32e95e97f72883a48e93
Yordanos Dereje <yordadereje@gmail.com>
2022-03-21 07:58:57 +0300

    first task

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include<main.h>
+/**
+ * swap_int - function to swap two numbers
+ * @*a - parameter 1
+ * @*b - parameter 2
+ * return: nothing
+ */
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
