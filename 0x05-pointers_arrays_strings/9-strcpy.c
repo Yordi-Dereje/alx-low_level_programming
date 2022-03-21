97b621e9014492976d749e8e843ddf699e9dc5de
Yordanos Dereje <yordadereje@gmail.com>
2022-03-21 18:16:41 +0300

    9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,14 +11,14 @@
 char *_strcpy(char *dest, char *src)
 {
 	int a;
-	int b;
+	int b = 0;
 
 	for (a = 0; src[a] != '\0'; a++)
 		b++;
-	
+
 	for (a = 0; a < b; a++)
 		dest[a] = src[a];
 	dest[b] = '\0';
-	
+
 	return (dest);
 }
