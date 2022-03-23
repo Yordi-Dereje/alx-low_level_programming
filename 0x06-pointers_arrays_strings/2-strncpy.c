c39cf4fc7fb74809af8a326663348d7df40101d2
Yordanos Dereje <yordadereje@gmail.com>
2022-03-23 19:55:34 +0300

    2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strncpy - a function to copy string src to dest
+ *
+ * @dest: destination string
+ *
+ * @src: source string
+ *
+ * return: the pasted dest string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int a = 0;
+
+	while (a < n && src[a] != '\0')
+	{
+		dest[a] = src[a];
+		a++
+	}
+	dest[a] = '\0';
+	return (dest);
+}
