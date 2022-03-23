582c9b503bb2b2545a0d02e2b5a831b2f31675b7
Yordanos Dereje <yordadereje@gmail.com>
2022-03-23 20:52:12 +0300

    3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strcmp - a function that compares 2 strings
+ *
+ * @s1: string 1
+ *
+ * @s2: string 2
+ *
+ * return: the answer ig
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int a = 0;
+
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	return (0);
+}
