e0fd6bc8f08768ca44e245882f3a0d20e97dae32
Yordanos Dereje <yordadereje@gmail.com>
2022-03-24 20:14:22 +0300

    102

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_number - a function that prints an integer
+ *
+ * @n: the passed integer
+ *
+ * Return: nothing
+ */
+void print_number(int n)
+{
+	_putchar(n);
+}
