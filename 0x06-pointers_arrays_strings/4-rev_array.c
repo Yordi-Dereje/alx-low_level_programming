e19132d717a9f8df8ad5f3d6047ac929c717a9ca
Yordanos Dereje <yordadereje@gmail.com>
2022-03-24 19:27:36 +0300

    4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -13,10 +13,10 @@ void reverse_array(int *a, int n)
 	int i;
 	int temp;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n--; i++)
 	{
 		temp = a[i];
-		a[i] = a[n - i];
-		a[n - i] = temp;
+		a[i] = a[n];
+		a[n] = temp;
 	}
 }
