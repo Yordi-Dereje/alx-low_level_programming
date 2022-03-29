fb1fb5528a01d04b553ff4af043d15713e80253b
Yordanos Dereje <yordadereje@gmail.com>
2022-03-29 20:26:12 +0300

    888888

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -12,11 +12,11 @@ void print_diagsums(int *a, int size)
 	int i;
 	int sum1 = 0;
 	int sum2 = 0;
-
-	for (i = 0; i < size * size; i += (size + 1))
+	int newSize = size * size;
+	for (i = 0; i < newSize; i += (size + 1))
 		sum1 += a[i];
 
-	for (i = size - 1; i < (size * size); i += (size - 1))
-		sum2 += [i];
+	for (i = size - 1; i < newSize - 1; i += (size - 1))
+		sum2 += a[i];
 	printf("%d, %d\n", sum1, sum2);
 }
