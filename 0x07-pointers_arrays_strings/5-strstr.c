848c5837fe93d9f6aefde33b9c6fb3fbde03384f
Yordanos Dereje <yordadereje@gmail.com>
2022-03-29 20:20:41 +0300

    5555

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,16 +11,19 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i;
 	int j;
+	int k = 0;
 
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		for (j = 0; needle[j] != '\0'; j++)
+		j = 0;
+		k = i;
+		while (haystack[k] == needle[j] && needle[j] != '\0')
 		{
-			if (haystack[j] != needle[j])
-				break;
-		}
+			j++;
+			k++;
+		}	
+		if (needle[j] != '\0')
+			return (haystack + i);
 	}
-	if (haystack[i] != '\0')
-		return (haystack + i);
-	return (NULL);
+	return (0);
 }
