aa1474143ec0d540839a672979d7792c478fbdc3
Yordanos Dereje <yordadereje@gmail.com>
2022-03-29 20:09:40 +0300

    1111

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,12 +9,12 @@
  */
 char *_strchr(char *s, char c)
 {
-	unsigned int i;
+	int i;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (c == s[i])
-			return (&s[i]);
+		if (s[i] == c)
+			return (s + i);
 	}
 	return (NULL);
 }
