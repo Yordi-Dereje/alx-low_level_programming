bc62994fbddc6a8d040d11500c5af9bd5ab3c554
Yordanos Dereje <yordadereje@gmail.com>
2022-03-30 18:16:41 +0300

    0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,9 +1,7 @@
 #include "main.h"
 /**
  * _puts_recursion - a function that prints a string followed by a new line
- * 
  * @s: the passed string
- *
  * Return: nothing
  */
 void _puts_recursion(char *s)
