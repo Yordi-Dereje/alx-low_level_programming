206c73c61d8003188ae34daaae5db1be52307d46
Yordanos Dereje <yordadereje@gmail.com>
2022-04-01 19:45:02 +0300

    4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -33,14 +33,14 @@ int main(int argc, char *argv[])
 
 	for (i = 1; i < argc; i++)
 	{
-		if(isdigit(argv[i]))
-			a = a + atoi(argv[i]);
+		if (isdigit(argv[i]))
+			a += atoi(argv[i]);
 		else
 		{
 			printf("Error\n");
 			return (1);
 		}
 	}
-	printf("%d\n",a);
+	printf("%d\n", a);
 	return (0);
 }
