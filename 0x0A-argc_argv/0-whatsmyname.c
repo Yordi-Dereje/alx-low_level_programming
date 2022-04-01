aee627a130e247ce98de475c5af6a457375dac6f
Yordanos Dereje <yordadereje@gmail.com>
2022-04-01 19:35:42 +0300

    master to main

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - the function to print the name of a program
+ * @argc: the count
+ * @argv: the array
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	argc++;
+	printf(%s\n, argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+/**
+ * isdigit - checks if the recieved values are numbers
+ * @s: input string
+ * Return: 0 if not digit, 1 if so
+ */
+int isdigit(char *s)
+{
+	int i = 0;
+	int j = strlen(s);
+
+	while (i < j)
+	{
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+		else
+			i++;
+	}
+	return (1);
+}
+/**
+ * main - adds positive numbers
+ * @argc: the count
+ * @argv: the passed argument
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int a = 0;
+	int i;
+
+	for (i = 1; i < argc; i++)
+	{
+		if(isdigit(argv[i]) == 1)
+			a = a + atoi(argv[i]);
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	printf("%d\n",a);
+	return (0);
+}
