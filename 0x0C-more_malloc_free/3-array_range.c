47d6fd4c2ecdd77b01a9a3727b588afd2d308c42
Yordanos Dereje <yordadereje@gmail.com>
2022-04-07 22:03:42 +0300

    betty for 3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -15,7 +15,7 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 	size = max - min + 1;
-	a = (int *)malloc(size*sizeof(int));
+	a = (int *)malloc(size * sizeof(int));
 	if (a == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
