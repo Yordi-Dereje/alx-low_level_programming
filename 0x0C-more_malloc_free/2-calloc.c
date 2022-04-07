1da2c417ba3b7cd708fb575a32a98f2df84af03c
Yordanos Dereje <yordadereje@gmail.com>
2022-04-07 22:02:48 +0300

    betty again

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * _calloc: a function that allocates memory for an array using malloc
+ * _calloc - a function that allocates memory for an array using malloc
  * @nmemb: the passed array
  * @size: byte size
  * Return: a pointer to the allocated memory or null
