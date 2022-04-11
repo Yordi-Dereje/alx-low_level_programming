9085039ba5fd05026e036e871de6582c153d9e92
Yordanos Dereje <yordadereje@gmail.com>
2022-04-11 14:08:31 +0300

    doct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,11 +2,17 @@
 #define DOG_H
 
 #include <stdio.h>
+/**
+ * struct dog - a structure for dog entity
+ * @name: attribute one
+ * @age: attribute two
+ * @owner: attribute three
+ */
 struct dog
 {
-	char* name;
+	char *name;
 	float age;
 	char *owner;
-}
+};
 
 #endif
