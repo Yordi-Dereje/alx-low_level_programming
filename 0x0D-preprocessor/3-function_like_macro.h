a35c586e7fb98a44b69a17a23c01058ef0631a5e
Yordanos Dereje <yordadereje@gmail.com>
2022-04-11 20:07:08 +0300

    3 3

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -2,6 +2,8 @@
 #define FUNCTION_LIKE_MACRO_H
 
 #include <stdio.h>
-#define ABS(x) ((x < 0) ? (x * -1) : (x))
+#define ABS(X) ((X < 0) ? (X * -1) : (X))
+ABS(X)
+	==> ((x < 0) ? (x * -1) : (x))
 
 #endif
