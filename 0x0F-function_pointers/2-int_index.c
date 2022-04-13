96e74568a4ae194472cec2fbb691c092175ef510
Yordanos Dereje <yordadereje@gmail.com>
2022-04-13 19:20:07 +0300

    2 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -5,7 +5,7 @@
  * @array: the passed values
  * @size: the number of elements in the array
  * @cmp: pointer to a function used to compare values
- * Return: no match -1, size less than 0 -1 and 0 if successful
+ * Return: -1 for no match, -1 for size less than 0 and if successful 0
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
@@ -16,7 +16,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i] != 0)
-				return (i);
+			return (i);
 	}
 	return (-1);
 }
