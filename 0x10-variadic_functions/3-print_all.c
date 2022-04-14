2d4b6f841e9a5fa82ba727518a6143d52208f580
Yordanos Dereje <yordadereje@gmail.com>
2022-04-14 20:06:45 +0300

    3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,4 +1,5 @@
-#include <stdarg>
+#include <stdarg.h>
+#include <stdio.h>
 #include "variadic_functions.h"
 /**
  * print_all - a function that prints anything
@@ -7,5 +8,40 @@
  */
 void print_all(const char * const format, ...)
 {
+	va_list args;
+	unsigned int i = 0;
+	char *s;
+	char *separator;
 
+	va_start(args, format);
+
+	separator = "";
+	while (format && format[i])
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%s%c", separator, va_arg(args, unsigned int));
+				break;
+			case 'i':
+				printf("%s%d", separator, va_arg(args, unsigned int));
+				break;
+			case 'f':
+				printf("%s%f", separator, va_arg(args, double));
+				break;
+			case 's':
+				s = va_arg(args, char *);
+				if (s == NULL)
+					s = "(nil)";
+				printf("%s%s", separator, s);
+				break;
+			default:
+				i++;
+				continue;
+		}
+		separator = ", ";
+		i++;
+	}
+	printf("\n");
+	va_end(args);
 }
