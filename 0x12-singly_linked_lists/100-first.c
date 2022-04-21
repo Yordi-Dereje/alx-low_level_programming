ff3707bcd0af5de3a558afa4a7f41605e49e62eb
Yordanos Dereje <yordadereje@gmail.com>
2022-04-22 00:48:45 +0300

    100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * print - a function that prints a text before the main function is executed
+ * Return: nothing
+ */
+void print(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,7 +2,6 @@
 #define LISTS_H
 
 int _putchar(char c);
-#include <stddef.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
