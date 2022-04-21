960c9e0f0a17b90d8ec8a3c30d6b8c40b853e83a
Yordanos Dereje <yordadereje@gmail.com>
2022-04-22 00:51:34 +0300

    1000

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,8 +1,9 @@
-#include "lists.h"
+#include <stdio.h>
 /**
  * print - a function that prints a text before the main function is executed
  * Return: nothing
  */
+void print(void) __attribute__((constructor));
 void print(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
