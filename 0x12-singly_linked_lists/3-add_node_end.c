e7a042c7737c6aa491f4ede8f936513b82b8bfa9
Yordanos Dereje <yordadereje@gmail.com>
2022-04-22 00:17:23 +0300

    333

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -9,7 +9,7 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *n = malloc(sizeof(list_t));
+	list_t *n = (list_t *)malloc(sizeof(list_t));
 	list_t *l = *head;
 	int i = 0;
 
