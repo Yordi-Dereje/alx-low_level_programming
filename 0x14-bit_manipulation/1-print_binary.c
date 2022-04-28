c3358d56041c0deeacee7dc93aebf262d01e3dae
Yordanos Dereje <yordadereje@gmail.com>
2022-04-28 19:19:41 +0300

    0

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_binary - a function that prints the binary representation of a number
+ * @n: the passed number
+ * Return: nothing
+ */
+void print_binary(unsigned long int n)
+{
+	//okay can't use array, malloc or the arthmetics
+	//can't also use printf. So putchar it is
+}
