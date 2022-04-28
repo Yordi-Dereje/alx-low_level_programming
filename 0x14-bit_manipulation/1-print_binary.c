e516851f01570bd3d8144121e340838f169b3837
Yordanos Dereje <yordadereje@gmail.com>
2022-04-28 20:15:42 +0300

    1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,6 +6,16 @@
  */
 void print_binary(unsigned long int n)
 {
-	//okay can't use array, malloc or the arthmetics
-	//can't also use printf. So putchar it is
+	int i;
+	int j;
+
+	if (n == 0)
+		_putchar('0');
+	for (j = 0; i = sizeof(n) * 8 - 1; i >= 0; i--)
+	{
+		if ((n >> i) & 1)
+			j = 1;
+		if (j ==1)
+			((n >> i) & 1) ? _putchar('1') : _putchar('0');
+	}
 }
