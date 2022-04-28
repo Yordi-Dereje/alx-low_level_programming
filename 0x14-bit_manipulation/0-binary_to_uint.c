ecabb7303bb6a70864b1a7666b420ca037094f5c
Yordanos Dereje <yordadereje@gmail.com>
2022-04-28 19:39:28 +0300

    0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -21,10 +21,11 @@ unsigned int binary_to_uint(const char *b)
 			return (0);
 		count++;
 	}
-	for (i = count - 1; i >= 0; i--, x *= 2)
+	for (i = count - 1; i >= 0; i--)
 	{
+		x *= 2;
 		if (b[i] == '1')
 			a += x;
 	}
 	return (a);
-}	
+}
