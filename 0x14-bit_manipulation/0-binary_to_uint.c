68a29fdc5dfe20dc0d48415ee55cfb48f381f336
Yordanos Dereje <yordadereje@gmail.com>
2022-04-28 19:22:46 +0300

    master 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,15 +11,18 @@ unsigned int binary_to_uint(const char *b)
 	int count = 0;
 	int x = 1;
 	
+	if (b == NULL)
+		return (0);
 	for (i = 0; b[i] != '\0'; i++)
 	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
 		count++;
 	}
 	for (i = count; i >= 0; i--)
 	{
-		if (b[i] != 0 || b[i] != 1)
-			return (0);
-		a += atoi(b[i]) * x;
+		if (b[i] == '1')
+			a += x;
 		x *= 2;
 	}
 	return (a);
