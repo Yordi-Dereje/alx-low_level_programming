7cb488e918fbdc7a068fed3b317014f6d7ae02ca
Yordanos Dereje <yordadereje@gmail.com>
2022-05-02 18:42:15 +0300

    0 on main

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include <stdio.h>
-#include <stdlib>
+#include <stdlib.h>
 /**
  * read_textfile - a function that reads a text file and prints it to the
  * POSIX standard output
