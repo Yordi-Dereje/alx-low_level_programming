600735b4486df77f74a52f01671c5dd8f7d49bae
Yordi-Dereje <yordadereje@gmail.com>
2022-10-05 19:42:55 +0300

    1

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -6,5 +6,8 @@
 #include <math.h>
 
 int linear_search(int *array, size_t siz, int value);
+int binary_search(int *array, size_t size, int value);
+size_t midpoint(size_t start, size_t end);
+
 
 #endif
